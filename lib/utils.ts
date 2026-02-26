1ec121eddf3d8721778a6d6a94265644600b10fe
quacksire <sam@quacksire.dev>
2026-02-26 13:51:33 -0800

    test deploy hook

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,5 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
+
+// needed for shadcn/ui components that use `clsx` for class merging, but we want to use `tailwind-merge` to handle conflicts
