f8a93f35950f5b6c4f95e4d44915d4eec2922193
quacksire <sam@quacksire.dev>
2026-02-27 11:09:17 -0800

    feat: add CORS headers for API routes in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,20 @@ import type { NextConfig } from "next";
 initOpenNextCloudflareForDev();
 const nextConfig: NextConfig = {
   /* config options here */
+
+    // allow domains for api routes
+    async headers() {
+        return [
+            {
+                source: '/api/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Origin', value: '*' },
+                    { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
+                    { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+                ],
+            },
+        ];
+    }
 };
 
 export default nextConfig;
